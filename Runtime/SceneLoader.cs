931018148522e6aa8262a8c1ca3bf44295f0621c
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-10-23 23:41:15 +0300

    Update SceneLoader.cs

--- Runtime/SceneLoader.cs
+++ Runtime/SceneLoader.cs
@@ -6,14 +6,15 @@ using UnityEngine.SceneManagement;
 namespace SceneLoadSystem.Runtime
 {
     public class SceneLoader : MonoBehaviour
-    {   
-        [Header("Scene Load Settings")]  
-        [Space(10)]
-        [SerializeField] private bool _loadOnAwake = false;
+    {
+        [Header("Scene Load Settings")]
+        [Space(10)] 
+        [SerializeField] private bool _loadOnAwake = false;  
+        [SerializeField] private bool _unLoadSceneIfAlreadyLoaded = false;
         [SerializeField] private LoadSceneMode _loadSceneMode = LoadSceneMode.Additive;
         private Scene _loadedScene;
         private Coroutine _loadingCoroutine;
-        [SerializeField,HideInInspector]  private int _selectedSceneIndex = 0;
+        [SerializeField, HideInInspector] private int _selectedSceneIndex = 0;
         
         private Action OnSceneStartToLoad;
         private Action OnSceneLoadCompleted;
@@ -26,35 +27,39 @@ namespace SceneLoadSystem.Runtime
 
         private void Awake()
         {
-            if(!_loadOnAwake) return;
+            if (!_loadOnAwake) return;
             LoadScene();
         }
 
         public void LoadScene()
         {
-            if(_loadingCoroutine != null) return;
+            if (_loadingCoroutine != null) return;
             OnSceneStartToLoad?.Invoke();
-            _loadingCoroutine = StartCoroutine( LoadSceneAsync());
+            _loadingCoroutine = StartCoroutine(LoadSceneAsync());
         }
 
         private IEnumerator LoadSceneAsync()
         {
             yield return null;
+            if (_unLoadSceneIfAlreadyLoaded)
+                SceneManager.UnloadSceneAsync(_loadedScene); 
+            
             SceneManager.sceneLoaded += OnSceneLoaded;
             AsyncOperation operation = SceneManager.LoadSceneAsync(SelectedSceneIndex, _loadSceneMode);
             
             if (operation != null) yield break;
             Debug.LogError("Failed To Load: " + SelectedSceneIndex);
-            SceneManager.sceneLoaded -= OnSceneLoaded; 
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
-        {            
+        {
             _loadedScene = scene;
-            
+
             OnSceneLoadCompleted?.Invoke();
             SceneManager.sceneLoaded -= OnSceneLoaded;
             _loadingCoroutine = null;
         }
     }
 }
+
